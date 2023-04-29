d85095a364caea0588e70b6dbf60764b8adbb63c
DanhTranThaoNguyen <danhnguyen.st03@gmail.com>
2023-04-29 19:51:27 +0700

    cai dat doc truyen

--- web-frontend/src/views/readNovel.vue
+++ web-frontend/src/views/readNovel.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="contact" class="page">
-    <h4>Hiệu chỉnh Liên hệ</h4>
+    <h4>Gioi thieu noi dung</h4>
     <ContactForm
       :contact="contact"
       @submit:contact="updateContact"
